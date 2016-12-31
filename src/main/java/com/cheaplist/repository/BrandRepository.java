37ee9fb37bdd74ffb6fde777cec69f40639c06fa
Kywal <Kywal>
2016-12-31 16:51:28 +0100

    Suppression de Brand sur le serveur

--- src/main/java/com/cheaplist/repository/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cheaplist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cheaplist.model.Brand;
-
-public interface BrandRepository extends JpaRepository<Brand, Integer> {
-
-}
