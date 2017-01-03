67d37ff1ceb0ea28e20edf113348e309a54175e8
Sebastien G <Sebastien G>
2017-01-03 14:14:40 +0100

    Update Table Member/ShoppingList/Address

--- src/main/java/com/cheaplist/controller/GoogleController.java
+++ src/main/java/com/cheaplist/controller/GoogleController.java
@@ -33,78 +33,13 @@ public class GoogleController {
 	public String googleTest() {
 		RestTemplate restTemplate = new RestTemplate();
 		String Seb = new String();		
-		
-		List <String> Emblem = new ArrayList <String>();
-		Emblem.add("Auchan");
-		Emblem.add("Carrefour");
-		Emblem.add("Cora");
-		Emblem.add("Leclerc");
-		Emblem.add("Lidl");
-		Emblem.add("Match");
-		Emblem.add("Géant Casino");
-		
-		for (String Embleme : Emblem)
-		{
+			
 		  Seb = restTemplate.getForObject(
-	 "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=50.6138111,3.0423598999999997&radius=50000&types=grocery_or_supermarket&name="+Embleme+"&key=AIzaSyDizEEeL61KclC1OA9foAkA7SuNBxtFxsA",String.class);
+	 "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=50.6138111,3.0423598999999997&radius=50000&types=grocery_or_supermarket&name=&key=AIzaSyDizEEeL61KclC1OA9foAkA7SuNBxtFxsA",String.class);
 //		  System.out.println(Seb);
 		  
 		  // Attention JSON powaaaaaaaa
-		  
-		  try {
-			ObjectMapper mapper = new ObjectMapper();
-			JsonNode rootNode = mapper.readTree(new StringReader(Seb));
-			int value=0;
-			  for (JsonNode node : rootNode.path("results")) {
-				  System.out.println("Numéro: "+value+"  ID :"+node.path("id").asText());
-				  System.out.println("NOM :"+node.path("name").asText());
-				  String Adresse = node.path("vicinity").asText();
-
-				  
-				  String Rue;
-				try {
-					Rue = (Adresse.split(",")[Adresse.split(",").length-2]);
-				} catch (Exception e) { Rue="";
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				  String Ville = (Adresse.split(",")[Adresse.split(",").length-1]);
-				  System.out.println("Rue : "+Rue+"  Ville : "+ Ville);				  
-				  System.out.println("Lat :"+node.path("geometry").path("location").path("lat").asText()+"Long:"+node.path("geometry").path("location").path("lng").asText());
-				  value=value+1;
-				  System.out.println("\n");
-				  
-				  Shop Magasin = new Shop();
-				  Address Address_Magasin = new Address();
-				  Magasin.setName(node.path("name").asText());
-				  Magasin.setEmblem(Embleme);
-				  Magasin.setIsActive((byte) '1');
-				  Date date = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").parse("03-01-2017 11:30:00");
-				  Magasin.setCreatedDate(date);
-				  Magasin.setIdgoogle(node.path("id").asText());
-				  Address_Magasin.setStreetName(Rue);
-				  Address_Magasin.setCity(Ville);
-				  Address_Magasin.setLag(node.path("geometry").path("location").path("lat").asDouble());
-				  Address_Magasin.setLng(node.path("geometry").path("location").path("lng").asDouble());
-				//  Address_Magasin.setZipCode(0);				  
-				  Magasin.setAddress(Address_Magasin);
-				 shopService.create(Magasin);
-				  
-			  }
-		  
-		  
-		  
-		  
-		  
-		  
-		  } catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		}
+		
 		return Seb;
 	
 
