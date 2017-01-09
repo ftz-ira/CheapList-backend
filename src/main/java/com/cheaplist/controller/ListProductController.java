08439d94a31d3f5d794b2f996f7f4575c8bfe331
Sebastien G <Sebastien G>
2017-01-09 14:32:54 +0100

    Function Add/Delete Element on List

--- src/main/java/com/cheaplist/controller/ListProductController.java
+++ src/main/java/com/cheaplist/controller/ListProductController.java
@@ -2,7 +2,6 @@ package com.cheaplist.controller;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cheaplist.exception.ListProductNotFound;
 import com.cheaplist.model.ListProduct;
-import com.cheaplist.model.Member;
 import com.cheaplist.model.View;
 import com.cheaplist.service.ListProductService;
 import com.fasterxml.jackson.annotation.JsonView;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -33,19 +30,7 @@ public class ListProductController {
 
 	/*
 	 * @Autowired private ListProductValidator listProductValidator;
-	 * 
-	 * /*
-	 * 
-	 * @InitBinder private void initBinder(WebDataBinder binder) {
-	 * binder.setValidator(listProductValidator); }
-	 */
-
-	/*
-	 * @RequestMapping(value="/create", method=RequestMethod.GET) public
-	 * ModelAndView newListProductPage() { ModelAndView mav = new
-	 * ModelAndView("listProduct-new", "listProduct", new ListProduct()); return
-	 * mav; }
-	 */
+	*/
 
 	/***** GET ALL PRODUCT ID BY LIST ID ******/
 
@@ -94,13 +79,10 @@ public class ListProductController {
 				listProduct = listProductService.createOneElement(idList,idProduct,productQuantity);
 				System.out.println(listProduct.getId());
 				
-			} catch (JsonProcessingException e) {
+			} catch (IOException  e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
+			}
 		return listProduct;
 	}
 	
@@ -115,65 +97,6 @@ public class ListProductController {
 		listProductService.delete(listProduct.getId());
 		return new ResponseEntity<String>("ELEMENT DELETED",HttpStatus.OK);
 	}
-	
 
-	/*
-	 * @RequestMapping(value="/create", method=RequestMethod.POST) public
-	 * ModelAndView createNewListProduct(@ModelAttribute @Valid ListProduct
-	 * listProduct, BindingResult result, final RedirectAttributes
-	 * redirectAttributes) {
-	 * 
-	 * if (result.hasErrors()) return new ModelAndView("listProduct-new");
-	 * 
-	 * ModelAndView mav = new ModelAndView(); String message =
-	 * "New listProduct "+listProduct.getCity()+" was successfully created.";
-	 * 
-	 * listProductService.create(listProduct);
-	 * mav.setViewName("redirect:/index.html");
-	 * 
-	 * redirectAttributes.addFlashAttribute("message", message); return mav; }
-	 * 
-	 * @RequestMapping(value="/list", method=RequestMethod.GET) public
-	 * ModelAndView listProductListPage() { ModelAndView mav = new
-	 * ModelAndView("listProduct-list"); List<ListProduct> listProductList =
-	 * listProductService.findAll(); mav.addObject("listProductList",
-	 * listProductList); return mav; }
-	 * 
-	 * 
-	 * 
-	 * @RequestMapping(value="/edit/{id}", method=RequestMethod.GET) public
-	 * ModelAndView editListProductPage(@PathVariable Integer id) { ModelAndView
-	 * mav = new ModelAndView("listProduct-edit"); ListProduct listProduct =
-	 * listProductService.findById(id); mav.addObject("listProduct",
-	 * listProduct); return mav; }
-	 * 
-	 * @RequestMapping(value="/edit/{id}", method=RequestMethod.POST) public
-	 * ModelAndView editListProduct(@ModelAttribute @Valid ListProduct
-	 * listProduct, BindingResult result,
-	 * 
-	 * @PathVariable Integer id, final RedirectAttributes redirectAttributes)
-	 * throws ListProductNotFound {
-	 * 
-	 * if (result.hasErrors()) return new ModelAndView("listProduct-edit");
-	 * 
-	 * ModelAndView mav = new ModelAndView("redirect:/index.html"); String
-	 * message = "ListProduct was successfully updated.";
-	 * 
-	 * listProductService.update(listProduct);
-	 * 
-	 * redirectAttributes.addFlashAttribute("message", message); return mav; }
-	 * 
-	 * @RequestMapping(value="/delete/{id}", method=RequestMethod.GET) public
-	 * ModelAndView deleteListProduct(@PathVariable Integer id, final
-	 * RedirectAttributes redirectAttributes) throws ListProductNotFound {
-	 * 
-	 * ModelAndView mav = new ModelAndView("redirect:/index.html");
-	 * 
-	 * ListProduct listProduct = listProductService.delete(id); String message =
-	 * "The listProduct "+listProduct.getCity()+" was successfully deleted.";
-	 * 
-	 * redirectAttributes.addFlashAttribute("message", message); return mav; }
-	 * 
-	 */
 
 }
