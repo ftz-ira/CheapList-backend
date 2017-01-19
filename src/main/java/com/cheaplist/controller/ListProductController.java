397a13d84a97399b3fdc6c462daa6d3bf366bf71
Sebastien G <Sebastien G>
2017-01-19 10:34:07 +0100

    Fix Errors

--- src/main/java/com/cheaplist/controller/ListProductController.java
+++ src/main/java/com/cheaplist/controller/ListProductController.java
@@ -192,7 +192,7 @@ public class ListProductController {
 			String lat = rootNode.path("lat").asText();
 			String lng = rootNode.path("lng").asText();
 			String radius = "3500"; // Par defaut
-			String emblem = "Auchan|Carrefour|Cora|Leclerc|Lidl|Match|Casino";
+			String emblem = "Auchan|Carrefour|Cora|Leclerc|Lidl|Match|G�ant Casino";
 			String key = "AIzaSyDizEEeL61KclC1OA9foAkA7SuNBxtFxsA";
 
 			// On récupère la liste des magasins à partir du GPS du client
